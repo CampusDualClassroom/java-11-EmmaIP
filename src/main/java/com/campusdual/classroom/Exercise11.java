3bef982db600887dfe0101aec2e8c77e4c32b431
EmmaIP <emmapineiro@gmail.com>
2024-10-15 17:29:08 +0200

    ejercicio 11 finalizado

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,35 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+
+        redRemote.turnOn();
+        System.out.println("El mando está: " + redRemote.isOn());
+
+        redRemote.channelUp();
+        System.out.println("El canal es: " + redRemote.getChannel());
+
+        redRemote.channelDown();
+        System.out.println("El canal es: " + redRemote.getChannel());
+
+        redRemote.channel = 0;
+        System.out.println("El canal es: " + redRemote.getChannel());
+        redRemote.channelDown();
+        System.out.println("El canal es: " + redRemote.getChannel());
+
+        redRemote.volumeUp();
+        System.out.println("El volumen es: " + redRemote.getVolume());
+        redRemote.volumeDown();
+        System.out.println("El volumen es: " + redRemote.getVolume());
+
+        redRemote.volume = 0;
+        System.out.println("El volumen es: " + redRemote.getVolume());
+        redRemote.volumeDown();
+
+        System.out.println("El color del mando a distancia es: " + redRemote.getColor());
+
+        redRemote.turnOff();
+        System.out.println("El mando está: " + redRemote.isOn());
+
     }
 
 }
